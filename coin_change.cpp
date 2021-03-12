20836163e861c861f8ccc2441694b3f48a1f6bb2
RevDrSquash <61360454+RevDrSquash@users.noreply.github.com>
2021-03-12 09:39:22 -0800

    Update coin_change.cpp

--- coin_change.cpp
+++ coin_change.cpp
@@ -1,4 +1,4 @@
-// https://leetcode.com/explore/challenge/card/march-leetcoding-challenge-2021/589/week-2-march-8th-march-14th/3668/
+// https://leetcode.com/problems/coin-change/
 
 class Solution {
 public:
