a5d0fcf8f31118ab9c4f6322c5449ffdb69adbdd
RevDrSquash <61360454+RevDrSquash@users.noreply.github.com>
2021-03-15 17:15:24 -0700

    Create reducing_dishes.cpp
    https://leetcode.com/problems/reducing-dishes/

---
+++ reducing_dishes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    static bool decendingSort (int i,int j) { return (i>j); }
+    
+public:
+    int maxSatisfaction(vector<int>& satisfaction)
+    {
+        // sort satisfaction by decending value
+        std::sort(satisfaction.begin(), satisfaction.end(), decendingSort);
+        
+        int coefficient = 0;
+        int curSum = 0;
+        for(const int& score : satisfaction)
+        {
+            int newSum = curSum + score;
+            if(newSum > 0)
+            {
+                curSum = newSum;
+                coefficient += newSum;
+            }
+        }
+        
+        return coefficient;
+    }
+};
